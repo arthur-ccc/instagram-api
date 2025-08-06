d8742adbd227c993f168ec2e37ca4f3dee2eeb94
Arthur Fernandes Vieira <arthur.fernandes.vieira@ccc.ufcg.edu.br>
2025-08-06 06:34:12 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+const ACCESS_TOKEN = process.env.IG_ACCESS_TOKEN;
+const IG_USER_ID   = process.env.IG_USER_ID;
+
+module.exports = async (req, res) => {
+	const limit = 4
+
+	const url = `https://graph.facebook.com/v19.0/${IG_USER_ID}/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${ACCESS_TOKEN}&limit=${limit}`;
+	try {
+		const response = await fetch(url);
+		const data     = await response.json();
+
+		if (data.error) {
+			return res.status(400).json({ error: data.error });
+		}
+
+		return res.status(200).json({ data: data.data });
+	} catch (err) {
+		console.error("Erro ao buscar por posts:", err);
+		return res.status(500).json({ error: "Erro interno do servidor." });
+	}
+};
+
