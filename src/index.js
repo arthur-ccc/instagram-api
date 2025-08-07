ad15464b1c9ca81c0938203ec590106e8fa78bf0
Arthur Fernandes Vieira <arthur.fernandes.vieira@ccc.ufcg.edu.br>
2025-08-07 17:18:15 -0300

    fix: corrije erro de CORS

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ const ACCESS_TOKEN = process.env.IG_ACCESS_TOKEN;
 const IG_USER_ID   = process.env.IG_USER_ID;
 
 module.exports = async (req, res) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+	if (req.method === "OPTIONS") {
+		return res.status(200).end();
+	}
+
 	const limit = 4
 
 	const url = `https://graph.facebook.com/v19.0/${IG_USER_ID}/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token=${ACCESS_TOKEN}&limit=${limit}`;
